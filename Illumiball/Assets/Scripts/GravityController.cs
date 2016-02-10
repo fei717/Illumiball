318394c7900b5fab75312f0931f70037d104e8f0
fei717 <feitfe@gmail.com>
2016-02-10 23:50:39 +0900

    2016/02/10 Unity 3.4節終了

---
+++ Illumiball/Assets/Scripts/GravityController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController : MonoBehaviour {
+	// 重力加速度
+	const float Gravity = 9.81f;
+
+	// 重力の適用具合
+	public float gravityScale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vector = new Vector3 ();
+
+		// Unityエディタと実機で処理を分ける
+		if (Application.isEditor) {
+			// キーの入力を検知しベクトルを設定
+			vector.x = Input.GetAxis ("Horizontal");
+			vector.z = Input.GetAxis ("Vertical");
+
+			// 高さ方向の判定はキーのzとする
+			if (Input.GetKey ("z")) {
+				vector.y = 1.0f;
+			} else {
+				vector.y = -1.0f;
+			}
+		} else {
+			// 加速度センサの入力をUnity空間の軸にマッピングする
+			vector.x = Input.acceleration.x;
+			vector.y = Input.acceleration.z;
+			vector.z = Input.acceleration.y;
+		}
+
+		// シーンの重力を入力ベクトルの方向に合わせて変化させる
+		Physics.gravity = Gravity * vector.normalized * gravityScale;				
+	}
+}
---
+++ Illumiball/Assets/Scripts/Hole.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public string activeTag;
+
+	void OnTriggerStay ( Collider other ){
+		// コライダに触れているオブジェクトのRigitbodyコンポーネントを取得
+		Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+		// ボールがどの方向にあるかを計算
+		Vector3 direction = transform.position - other.gameObject.transform.position;
+		direction.Normalize ();
+
+		// タグに応じてボールに力を加える
+		if (other.gameObject.tag == activeTag) {
+			// 中心地点でボールを止めるために速度を減速させる
+			r.velocity *= 0.9f;
+
+			r.AddForce (direction * r.mass * 20.0f);
+		} else {
+			r.AddForce (-direction * r.mass * 80.0f);
+		}
+	}
+}
